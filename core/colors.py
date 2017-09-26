ce63aa809d5cf4166a66e52de033dd59cbc90841
TheNaterz <nate.caroe@risksense.com>
2017-09-26 16:45:55 -0600

    commiting this to my own fork

--- core/colors.py
+++ core/colors.py
@@ -44,9 +44,9 @@ class Colors(object):
         import os
         glyph = "#" if os.geteuid() == 0 else "$"
         last = state.split("/")[-1]
-        mini_state = [s[0:3] for s in state.split("/")[:-1]]
-        mini_state.append(last)
-        mini_state = "/".join(mini_state)
+        state = [s[0:3] for s in state.split("/")[:-1]]
+        state.append(last)
+        state = "/".join(state)
         return "%s%s: %s%s" % (self.colorize("(", [self.GREEN], isreadline),
                                  self.colorize("koadic", [self.BOLD], isreadline),
                                  self.colorize(mini_state, [self.CYAN], isreadline),
